f6e7f170f180e9409a35263e8e42d691bf455f6c
Tyler Benning <tbenning27@gmail.com>
2018-09-03 20:57:23 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.Now go build something great.Now go build something great.</p>
-    <Link to="/page-2/">Go to page 3</Link>
+    <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
